6c01e6c77f8a8e77e4e440bb538e8f0799e97e33
Benjamin Kotey <koteyben1@gmail.com>
2021-08-26 21:41:34 -0400

    Almost done(Person 2)

---
+++ src/utils/Foo.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'ben1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("The things i do for homework");
+	}
+
+
+}
